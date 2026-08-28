e7ca0b4ef090d81d7dc3c5ec0b574abb0e7b4cf9
TienHM12 <minhtien250704@gmail.com>
2026-08-28 19:50:44 +0700

    feat(api): enable cors across services

--- apps/auth/src/main.ts
+++ apps/auth/src/main.ts
@@ -10,6 +10,11 @@ import { AppModule } from './app/app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
+  app.enableCors({
+    origin: '*',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
   app.setGlobalPrefix(globalPrefix);
   const port = process.env.PORT || 3000;
   await app.listen(port);
--- apps/chat/src/app/chat-gateway/chat-gateway.ts
+++ apps/chat/src/app/chat-gateway/chat-gateway.ts
@@ -10,7 +10,7 @@ import {
 } from '@nestjs/websockets';
 import { Server, Socket } from 'socket.io';
 
-@WebSocketGateway({ cors: true })
+@WebSocketGateway({ cors: { origin: '*' } })
 export class ChatGateway
   implements OnGatewayInit, OnGatewayConnection, OnGatewayDisconnect
 {
